c4170370b5bcf1d0276963935a8592fa4d00753e
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-10-12 01:03:35 +0600

    commit 9

--- src/component/blog/Blog.js
+++ src/component/blog/Blog.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Blog = () => {
  return (
   <div>
-   
+<h1>Progrmming Blog</h1>
+<div>
+<h2>What is purpose of react work?</h2>
+<p>Easy creation of dynamic applications: React makes it easier to create dynamic web applications because it requires less coding and offers more functionality, as opposed to JavaScript, where coding often gets complex very quickly. Improved performance: React uses Virtual DOM, thereby creating web applications faster</p>
+
+</div>
   </div>
  );
 };
